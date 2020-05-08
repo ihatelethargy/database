8de54151f0fa738a313d237515186b71a522edd8
ihatelethargy <naturalflavor1007@naver.com>
2020-05-08 14:19:41 +0900

    count(2);

--- worlddb.sql
+++ worlddb.sql
@@ -1,7 +1,15 @@
 USE world;
--- (1) 중국 도시수 계산
+-- (1) 중국 도시
 SELECT ci.ID, ci.Name 
 FROM city ci JOIN country co 
 ON ci.CountryCode = co.Code
 WHERE co.Name = 'China'; 
+
+-- 중국 도시 숫자
+SELECT COUNT(*) AS NumberOfCityInChina 
+FROM city ci
+JOIN country co
+ON ci.CountryCode = co.Code
+WHERE co.Name = 'China';
+
  
