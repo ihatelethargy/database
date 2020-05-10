5ad220acfe30d797055a867e22f08e00f8f2ebf8
ihatelethargy <naturalflavor1007@naver.com>
2020-05-10 23:36:10 +0900

    g

--- group.sql
+++ group.sql
@@ -2,8 +2,10 @@ USE pubs;
 
 -- (1) 현재 직원(employee)는 출판사에 관계없이 하나의 테이블에 저장되어 있다. 각 출판사 명 별 직원 수를 구하시오. 단 출판사 이름은 유일하지 않다고 가정합니다.
 
-SELECT p.pub_name, e.fname 
-FROM publishers p, employee e;
-
+SELECT p.pub_name , count(e.emp_id)
+FROM publishers p
+JOIN employee e
+ON p.pub_id = e.pub_id
+GROUP BY pub_name;
 
 -- GROUP BY p.pub_name;
